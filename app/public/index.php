4834d822794f1ff2ffb4fe560077a182c425cb05
Michael Anderson <33232231+MichaelAceAnderson@users.noreply.github.com>
2024-10-19 17:18:38 +0200

    Initial commit

---
+++ app/public/index.php
@@ -0,0 +1,6 @@
+Congratulations, you have successfully launched your PHP application in a Docker container!
+Feel free to check out <a href="testBDD.php">the test page</a> to see if your database container is working.
+<?php
+echo "<br>PHP executable file used: " . PHP_BINARY;
+phpinfo();
+?>
